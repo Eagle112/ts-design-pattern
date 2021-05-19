3982200e7c27a4705725c294935af255a0715fce
yinghongbin <yinghongbin@appledeMacBook-Pro.local>
2021-05-19 15:51:49 +0800

    123

--- run-ts.sh
+++ run-ts.sh
@@ -1,4 +1,10 @@
-#参数$1 为Prototype.
-echo "$1ts"
-tsc "$1ts"
-node "$1js"
+#! /bin/bash 
+OLD_IFS="$IFS"
+IFS="."
+arr=($1)
+if [ ${#arr[@]} == 2 ]
+  then tsc "$1" && node "${arr[0]}.js"
+else 
+  tsc "$1ts"
+  node "$1js"
+fi
